4c83f329588f818b6654d56a919cef3134f12bf6
robhyrk <rob.hyrkiel@gmail.com>
2019-10-17 10:13:23 -0400

    code for day 3

---
+++ src/partials/Ball.js
@@ -0,0 +1,70 @@
+import {SVG_NS} from '../settings'
+
+export default class Ball {
+    constructor(radius, boardWidth, boardHeight) {
+        this.radius = radius
+        this.boardWidth = boardWidth
+        this.boardHeight = boardHeight
+        this.direction = 1
+        this.reset()
+    }
+
+    wallCollision() {
+        const hitLeft = this.x - this.radius <= 0
+        const hitRight = this.x + this.radius >= this.boardWidth
+        const hitTop = this.y - this.radius <= 0
+        const hitBottom = this.y + this.radius >= this.boardHeight
+
+        if(hitLeft || hitRight) {
+            this.vx = -this.vx
+        } else if (hitTop || hitBottom) {
+            this.vy = -this.vy
+        }
+    }
+
+    paddleCollision(player1, player2) {
+        if(this.vx > 0) {
+            let paddle = player2.coordinates(player2.x, player2.y, player2.width, player2.height)
+            let [leftX, rightX, topY, bottomY ] = paddle
+            if(
+                (this.x + this.radius >= leftX) 
+                && (this.x + this.radius <= rightX)
+                && (this.y >= topY && this.y <= bottomY)
+            ) {
+                this.vx = -this.vx
+            }
+
+        } else {
+            // player 1 collision
+        }
+    }
+
+    reset() {
+        this.x = this.boardWidth / 2
+        this.y = this.boardHeight / 2
+
+        this.vy = 0
+        while( this.vy === 0 ) {
+            this.vy = Math.floor(Math.random() * 10 -5)
+        }
+
+        this.vx = this.direction * (6 - Math.abs(this.vy))
+        console.log()
+    }
+
+    render(svg, player1, player2) {
+        this.x += this.vx
+        this.y += this.vy
+
+        this.wallCollision()
+        this.paddleCollision(player1, player2)
+
+
+        let circle = document.createElementNS(SVG_NS, 'circle')
+        circle.setAttributeNS(null, 'r', this.radius)
+        circle.setAttributeNS(null, 'cx', this.x)
+        circle.setAttributeNS(null, 'cy', this.y)
+        circle.setAttributeNS(null, 'fill', 'white')
+        svg.appendChild(circle)
+    }
+}
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,4 +1,7 @@
-import {SVG_NS} from '../settings'
+import {SVG_NS, KEYS} from '../settings'
+import Board from './Board';
+import Paddle from './Paddle'
+import Ball from './Ball';
 
 export default class Game {
   constructor(element, width, height) {
@@ -8,16 +11,54 @@ export default class Game {
 
     this.gameElement = document.getElementById(this.element)
 
-		// Other code goes here...
+    //creates a new instance of board. height and width passed through from Game object
+    this.board = new Board(this.width, this.height)
+
+    this.paddleWidth = 8
+    this.paddleHeight = 56
+    this.boardGap = 10 //this is your x value for Paddle
+
+    //create ball
+    this.ball = new Ball(8, this.width, this.height)
+
+    //creates Player 1 paddle
+    this.player1 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      this.boardGap,
+      ((this.height - this.paddleHeight) / 2),
+      KEYS.a,
+      KEYS.z
+    )
+    //create Player 2 paddle
+    this.player2 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      (this.width - this.boardGap - this.paddleWidth),
+      ((this.height - this.paddleHeight) / 2),
+      KEYS.up,
+      KEYS.down
+    )
+    console.log(this.board)
+
   }
 
   render() {
     let svg = document.createElementNS(SVG_NS, 'svg')
     
+    this.gameElement.innerHTML = ''
+
     svg.setAttributeNS(null, 'width', this.width)
     svg.setAttributeNS(null, 'height', this.height)
     svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`)
     this.gameElement.appendChild(svg)
 
+    this.board.render(svg)
+    this.player1.render(svg)
+    this.player2.render(svg)
+
+    this.ball.render(svg, this.player1, this.player2)
   }
 }
